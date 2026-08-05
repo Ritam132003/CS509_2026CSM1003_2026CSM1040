8014b611699fd53c658e7e4da7ba8df99b73f883
Ritam132003 <2026csm1003@iitrpr.ac.in>
2026-08-05 10:27:27 +0530

    graph

---
+++ Assignment_01/src/bfs.h
@@ -0,0 +1,23 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+typedef struct
+{
+    int V;
+    int E; 
+
+    int *row_ptr;   // CSR row pointer
+    int *col_idx;   // CSR column indices
+    int *values;    // Edge weights (used by SSSP)
+} CSRGraph;
+
+// Read adjacency list and convert to CSR
+CSRGraph* loadGraphCSR(const char *filename, int weighted);
+
+// Print CSR arrays (for debugging)
+void printCSR(CSRGraph *graph);
+
+// Free allocated memory
+void freeGraph(CSRGraph *graph);
+
+#endif
